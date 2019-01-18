f7fcc4e95d15b8cb2fbe80a2e9de924f214dc8f6
fl <fen.lombardi@gmail.com>
2019-01-18 15:35:23 -0800

    Add 2 additional questions, add styling to button, add links to epicodus pages for results.

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,34 @@ $(document).ready(function() {
     var who = parseInt($("#who").val());
     var what = parseInt($("#what").val());
     var how = parseInt($("#how").val());
+    var motive = parseInt($("#motive").val());
+    var nick = parseInt($("#nick").val());
     console.log('name:' + nameInput)
     console.log('who:' + who)
     console.log('how:' + how)
     console.log('what:' + what)
+    console.log('motive:' + motive)
+    console.log('nick:' + nick)
 
-    if (who + what + how >= 3) {
+    if (who + what + how + motive + nick >= 5) {
       $("#c").show();
       $("#java").hide();
       $("#php").hide();
       $("#ruby").hide();
     }
-    if (who + what + how >= 7) {
+    if (who + what + how+ motive + nick >= 10) {
       $("#c").hide();
       $("#java").show();
       $("#php").hide();
       $("#ruby").hide();
     }
-    if (who + what + how >= 9) {
+    if (who + what + how+ motive + nick >= 15) {
       $("#c").hide();
       $("#java").hide();
       $("#php").show();
       $("#ruby").hide();
     }
-    if (who + what + how >= 10) {
+    if (who + what + how+ motive + nick >= 20) {
       $("#c").hide();
       $("#java").hide();
       $("#php").hide();
