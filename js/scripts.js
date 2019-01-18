6c50b3b0b70666bad0a1a6bae06bbf7517d5b6e2
fl <fen.lombardi@gmail.com>
2019-01-18 09:34:33 -0800

    Add radio buttons and drop down to compare.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var nameInput = $("input#name").val();
+
+//var who = $("input:radio[name=who]:checked").val();
+
+var beverage = $("#who").val();
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $(".person1").text(person1Input);
+  $("form#tracks").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+
+    $("#output").text(result);
+  });
+});
