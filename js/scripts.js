74bbfa40efbad5877d6d83d43a438cbfdb81178f
fl <fen.lombardi@gmail.com>
2019-01-18 10:39:47 -0800

    Rewrite js script, name input working, first question not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,21 @@
-var nameInput = $("input#name").val();
-
-//var who = $("input:radio[name=who]:checked").val();
-
-var beverage = $("#who").val();
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
 $(document).ready(function() {
-  $(".person1").text(person1Input);
   $("form#tracks").submit(function() {
-    event.preventDefault();
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var operator = $("input:radio[name=operator]:checked").val();
-    var result = add(number1, number2);
-    if (operator === "add") {
-      result = add(number1, number2);
-    } else if (operator === "subtract") {
-      result = subtract(number1, number2);
-    } else if (operator === "multiply") {
-      result = multiply(number1, number2);
-    } else if (operator === "divide") {
-      result = divide(number1, number2);
+    var nameInput = $("input#name").val();
+    var who = parseInt($("#who").val());
+    var result;
+    if (who === "1") {
+      result = "#c";
+    } else if (who === "2") {
+      result = "#java";
+    } else if (who === "3") {
+      result = "#php";
+    } else if (who === "4") {
+      result = "#ruby";
     }
+    $(".name").text(nameInput);
 
-    $("#output").text(result);
+    event.preventDefault();
+
+    $("#output").show();
   });
 });
